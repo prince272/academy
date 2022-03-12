0cf5d8745c1faeee634bbb81aefaa6043e03f3ef
Prince Owusu <princeowusu.272@gmail.com>
2022-03-12 16:59:22 +0000

    Commit 5.

--- src/Academy.Web/pages/_app.js
+++ src/Academy.Web/pages/_app.js
@@ -4,7 +4,7 @@ import '../styles/app.css';
 
 import App from 'next/app';
 import Head from 'next/head';
-import { DefaultSeo  } from 'next-seo';
+import { DefaultSeo } from 'next-seo';
 import _ from 'lodash';
 import { Toaster } from 'react-hot-toast';
 import { ClientProvider, createHttpClient } from '../utils/client';
@@ -19,11 +19,6 @@ import ErrorPage from '../components/ErrorPage';
 
 export default function MyApp({ Component, pageProps, appSettings, error }) {
 
-  if (error) {
-    return (<ErrorPage {...{ error }} />)
-  }
-
-
   const pageSettings = Object.assign({}, {
     showHeader: true,
     showFooter: true
@@ -34,33 +29,41 @@ export default function MyApp({ Component, pageProps, appSettings, error }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       </Head>
-      <DefaultSeo 
-        titleTemplate ="%s | Academy Of Ours"
+      <DefaultSeo
+        titleTemplate="%s | Academy Of Ours"
         defaultTitle="Academy Of Ours"
         description="Academy of Ours is an e-learning platform that helps you to learn a variety of courses and concepts through interactive checkpoints, lessons, and videos."
+        openGraph={{
+          type: 'website',
+          locale: 'en_IE',
+          url: 'https://www.url.ie/',
+          site_name: 'Academy Of Ours',
+        }}
       />
-      <SSRProvider>
-        <EventDispatcherProvider>
-          <AppSettingsProvider {...{ appSettings }}>
-            <ClientProvider>
-              <DialogProvider>
-                <ModalProvider>
-                  <div className="pt-8 pb-5 position-relative">
-                    {pageSettings.showHeader && <Header />}
-                    <Body>
-                      <Component {...pageProps} />
-                    </Body>
-                    {pageSettings.showFooter && <Footer />}
-                  </div>
-                  <Toaster position="top-center" reverseOrder={true} toastOptions={{
-                    className: 'bg-light text-dark',
-                  }} />
-                </ModalProvider>
-              </DialogProvider>
-            </ClientProvider>
-          </AppSettingsProvider>
-        </EventDispatcherProvider>
-      </SSRProvider>
+      {!error ? (
+        <SSRProvider>
+          <EventDispatcherProvider>
+            <AppSettingsProvider {...{ appSettings }}>
+              <ClientProvider>
+                <DialogProvider>
+                  <ModalProvider>
+                    <div className="pt-8 pb-5 position-relative">
+                      {pageSettings.showHeader && <Header />}
+                      <Body>
+                        <Component {...pageProps} />
+                      </Body>
+                      {pageSettings.showFooter && <Footer />}
+                    </div>
+                    <Toaster position="top-center" reverseOrder={true} toastOptions={{
+                      className: 'bg-light text-dark',
+                    }} />
+                  </ModalProvider>
+                </DialogProvider>
+              </ClientProvider>
+            </AppSettingsProvider>
+          </EventDispatcherProvider>
+        </SSRProvider>
+      ) : (<ErrorPage {...{ error }} />)}
     </>
   );
 }
