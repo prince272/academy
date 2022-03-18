57738004f8d3703bdf88afd907e4e676592ed6a1
Prince Owusu <princeowusu.272@gmail.com>
2022-03-18 15:35:47 +0000

    Commit.

--- src/Academy.Web/pages/courses/[courseId]/learn/[...all].js
+++ src/Academy.Web/pages/courses/[courseId]/learn/[...all].js
@@ -375,7 +375,7 @@ const LearnPage = withRemount(({ remount }) => {
     }, [client.loading, client.user]);
 
     useEffect(() => {
-         load();
+        load();
     }, []);
 
     const moveBackward = () => {
@@ -431,19 +431,33 @@ const LearnPage = withRemount(({ remount }) => {
 
                 const comparator = (a, b) => {
                     return a.localeCompare(b, 'en', { numeric: true, sensitivity: 'base' })
-                  };
+                };
+
+                const sequenceEqual = (a, b) => {
+                    if (a === b) return true;
+                    if (a == null || b == null) return false;
+                    if (a.length !== b.length) return false;
+
+                    // If you don't care about the order of the elements inside
+                    // the array, you should sort both arrays here.
+                    // Please note that calling sort on an array will modify that array.
+                    // you might want to clone your array first.
+
+                    for (var i = 0; i < a.length; ++i) {
+                        if (a[i] !== b[i]) return false;
+                    }
+                    return true;
+                };
 
                 if (currentView.type == 'selectSingle' || currentView.type == 'selectMultiple') {
                     const checkedIds = answers.filter(answer => answer.checked).map(answer => answer.id.toString()).sort(comparator);
                     const inputIds = inputs.map(inputId => inputId.toString()).sort(comparator);
-                    console.log(checkedIds)
-                    console.log(inputIds)
-                    return checkedIds.every((checkId, checkIndex) => checkId == inputIds[checkIndex]);
+                    return sequenceEqual(checkedIds, inputIds);
                 }
                 else if (currentView.type == 'reorder') {
                     const checkedIds = answers.map(answer => answer.id.toString());
                     const inputIds = inputs.map(inputId => inputId.toString());
-                    return checkedIds.every((checkId, checkIndex) => checkId == inputIds[checkIndex]);
+                    return sequenceEqual(checkedIds, inputIds);
                 }
                 else {
                     return false;
