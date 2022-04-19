26b929ec805adb33944f0f0f3d125691eccd2757
Prince Owusu <princeowusu.272@gmail.com>
2022-04-19 22:11:48 +0000

    Commit.

--- src/Academy.Web/pages/courses/[courseId]/index.js
+++ src/Academy.Web/pages/courses/[courseId]/index.js
@@ -136,7 +136,7 @@ const LessonItem = ({ course, section, lesson, toggler, permitted }) => {
         <Draggable draggableId={`lesson_${lesson.id}`} index={lesson.index}>
             {(provided) => (
                 <div ref={provided.innerRef} {...provided.draggableProps} className="pb-3">
-                    <Scroll.Element name={`lesson_${lesson.id}`} className={`card text-body bg-white ${disabled ? 'opacity-75' : 'btn-outline-primary'}`}>
+                    <Scroll.Element name={`lesson_${lesson.id}`} className={`card text-body bg-white ${disabled ? 'opacity-50' : 'btn-outline-primary'}`}>
                         <div className="p-0 d-flex justify-content-between align-items-stretch border-bottom-0" style={{ height: "72px" }}>
                             <div className="p-2 d-flex align-items-center hstack gap-2">
                                 <div {...provided.dragHandleProps} className={`${!(permitted) && 'd-none'}`}>
@@ -173,7 +173,7 @@ const LessonItem = ({ course, section, lesson, toggler, permitted }) => {
                                         }
                                     </div>
                                 </div>
-                                <div className={`p-0 d-flex align-items-center rounded-pill text-white bg-${lesson.status == 'completed' ? 'success' : lesson.status == 'started' ? 'primary' : 'dark'}`}>
+                                <div className={`p-0 d-flex align-items-center rounded-pill text-white bg-${lesson.status == 'completed' ? 'success' : lesson.status == 'started' ? 'primary' : 'dark bg-opacity-25'}`}>
                                     <div className={`rounded-pill d-flex justify-content-center align-items-center`} style={{ height: "28px", width: "28px" }}>
                                         <span className="svg-icon svg-icon-xs d-inline-block" >{lesson.status == 'completed' ? <BsCheck2 /> : lesson.status == 'started' ? (course.price > 0 && !course.purchased ? <BsCart3 /> : <BsPlayFill />) : <BsLockFill />}</span>
                                     </div>
