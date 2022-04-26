62d9a6ee77c8c1d7e5c2327d3320c450bb5756e0
Prince Owusu <princeowusu.272@gmail.com>
2022-04-26 10:11:26 +0000

    Commit.

--- src/Academy.Web/pages/_document.js
+++ src/Academy.Web/pages/_document.js
@@ -17,7 +17,7 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-          <link rel="manifest" href="manifest.json" />
+          <link rel="manifest" href="/manifest.json" />
           <link rel="mask-icon" color="#166fe5" href="/favicon/safari-pinned-tab.svg" />
           <link rel="shortcut icon" href="/favicon/favicon.ico" />
           <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
