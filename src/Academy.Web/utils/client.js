8e8ee5a63fb047dbfad6715c3a30e17599251d60
Prince Owusu <princeowusu.272@gmail.com>
2022-04-26 11:38:40 +0000

    Commit.

--- src/Academy.Web/utils/client.js
+++ src/Academy.Web/utils/client.js
@@ -35,7 +35,8 @@ const createHttpClient = (defaultConfig, ctx) => {
         else {
             try {
                 const response = (await http.request(requestConfig)).data;
-                delete requestConfig.paramsSerializer
+                delete requestConfig.paramsSerializer;
+                delete requestConfig.httpsAgent;
                 return { ...response, data: { ...response.data, requestConfig } };
             }
             catch (ex) {
