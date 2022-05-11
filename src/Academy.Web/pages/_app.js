2ea6278a09a74934997faec5a21007bb165feec4
Prince Owusu <princeowusu.272@gmail.com>
2022-05-11 21:44:54 +0000

    Commit.

--- src/Academy.Web/pages/_app.js
+++ src/Academy.Web/pages/_app.js
@@ -429,6 +429,7 @@ export default function MyApp({ Component, pageProps, appSettings, error }) {
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
       <Script
+        id={_.uniqueId('gtag-')}
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
