5c14137b334c6f8c797d5e1dc7519ece254dc478
Prince Owusu <32231909+prince272@users.noreply.github.com>
2022-06-01 17:58:20 +0000

    Commit.

--- src/Academy.Web/components/DocumentViewer.js
+++ src/Academy.Web/components/DocumentViewer.js
@@ -2,6 +2,7 @@ import dynamic from 'next/dynamic';
 
 const DynamicDocumentViewer = dynamic(() => import('../components/DynamicDocumentViewer'), {
     ssr: false,
+    
 });
 
 const DocumentViewer = props => {
--- src/Academy.Web/components/DynamicDocumentViewer.js
+++ src/Academy.Web/components/DynamicDocumentViewer.js
@@ -16,6 +16,8 @@ import { pascalCase } from 'change-case';
 
 import { arrayMove, formatNumber, preventDefault, sleep, stripHtml } from '../utils/helpers';
 import { Tab } from 'react-bootstrap';
+import "froala-editor/css/froala_editor.pkgd.min.css";
+import "froala-editor/css/froala_style.css";
 import FroalaEditorView from 'react-froala-wysiwyg/FroalaEditorView';
 
 class IFrame extends React.Component {
