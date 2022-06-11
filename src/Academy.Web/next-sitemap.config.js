34d224b1b31b9574f58239810f83a78ff8b656b4
Prince Owusu <32231909+prince272@users.noreply.github.com>
2022-06-11 16:22:28 +0000

    Commit.

--- src/Academy.Web/next-sitemap.config.js
+++ src/Academy.Web/next-sitemap.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_CLIENT_URL,
   generateRobotsTxt: true,
+  sitemapBaseFileName: 'client-sitemap',
   exclude: [
-    '/index-sitemap.xml',
+    '/server-sitemap.xml',
     '/courses/sitemap.xml',
     '/posts/sitemap.xml'
   ],
   robotsTxtOptions: {
     additionalSitemaps: [
-      `${process.env.NEXT_PUBLIC_CLIENT_URL}/index-sitemap.xml`,
+      `${process.env.NEXT_PUBLIC_CLIENT_URL}/server-sitemap.xml`,
       `${process.env.NEXT_PUBLIC_CLIENT_URL}/courses/sitemap.xml`,
       `${process.env.NEXT_PUBLIC_CLIENT_URL}/posts/sitemap.xml`,
     ],
